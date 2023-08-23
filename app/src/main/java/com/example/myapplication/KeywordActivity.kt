5a6dc00038af992a66eb35b4a894b7e074efab2d
Kayeon Seon <gyseon20@duksung.ac.kr>
2023-08-23 21:49:48 +0900

    탭 3 키워드 등록

--- app/src/main/java/com/example/myapplication/KeywordActivity.kt
+++ app/src/main/java/com/example/myapplication/KeywordActivity.kt
@@ -2,6 +2,9 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import com.example.myapplication.databinding.ActivityKeywordBinding
 import com.example.myapplication.databinding.ActivityMainBinding
 
@@ -12,6 +15,41 @@ class KeywordActivity : AppCompatActivity() {
         val binding = ActivityKeywordBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 뒤로가기 버튼 창 종료
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
 
+        // 키워드 등록 버튼
+        val keywordTextArray = arrayOfNulls<String>(6)
+        // 6번 제한 카운트 변수
+        var count: Int = 0
+
+        binding.keywordBtn.setOnClickListener {
+
+            keywordTextArray[count] = binding.keywordEditTV.text.toString() // 배열에 입력값 저장
+//            keywordTextArray[0]?.let { Log.d("mobileApp", it) }
+
+            if (count == 0) {
+                binding.keywordTV1.text = keywordTextArray[count]
+            }
+            else if (count == 1) {
+                binding.keywordTV2.text = keywordTextArray[count]
+            }
+            else if (count == 2) {
+                binding.keywordTV3.text = keywordTextArray[count]
+            }
+            else if (count == 3) {
+                binding.keywordTV4.text = keywordTextArray[count]
+            }
+            else if (count == 4) {
+                binding.keywordTV5.text = keywordTextArray[count]
+            }
+            else if (count == 5){
+                binding.keywordTV6.text = keywordTextArray[count]
+                binding.keywordBtn.isEnabled = false
+            }
+//            Log.d("mobileApp", binding.keywordTV1.text.toString())
+        }
     }
 }
